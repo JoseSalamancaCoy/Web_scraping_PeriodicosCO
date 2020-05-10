1c06c97a093fc87b75da3897c8ef989d820525dc
JoseSalamancaCoy <jose.salamancacoy@gmail.com>
2020-05-09 19:39:28 -0500

    Comentarios base

--- common.py
+++ common.py
@@ -4,7 +4,7 @@ __config = None
 
 def config():
     global __config
-    if not __config:
+    if not __config: #Solo accede a disco si no ha sido cargado el archivo de config.yaml
         with open("config.yaml",mode='r') as f:
             __config = yaml.load(f)
     return __config
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-import argparse
-import logging
-logging.basicConfig(level=logging.INFO)
-from common import config
+import argparse  #Para crear argumentos en el ejutable
+import logging #Imprimir informacion al Usuario
+logging.basicConfig(level=logging.INFO) #Nivel de informacion
+
+from common import config #Archivo con la configuracion de los sitios WEB
 
 logger = logging.getLogger(__name__)
 
 def _news_scraper(news_site_uid):
-    host = config()['news_sites'][news_site_uid]['url']
+    host = config()['news_sites'][news_site_uid]['url'] #Se obtiene la URL del sitio WEB solitado
     logging.info('IniciandoScraper para {}'.format(host))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    new_site_choices = list(config()["news_sites"].keys())
+    new_site_choices = list(config()["news_sites"].keys()) #Obtiene lista de opciones para scraper
 
-    parser.add_argument('news_sites',
+    parser.add_argument('news_sites', 
                         help = 'Los Sitios para scraper',
                         type= str,
-                        choices=new_site_choices)
+                        choices=new_site_choices) #Agrega argumentos para ejecucion del usuario
     args = parser.parse_args()
-    print(args.news_sites)
-    _news_scraper(args.news_sites)
+    _news_scraper(args.news_sites) 
